4ab51eaa637e3362b2832fe3243ebeed4621bde2
Fernandopala <fernandopalavecino18@gmail.com>
2020-10-23 23:31:30 -0300

    cambios pencas

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Libro; // arreglar la ubicacion del modelo
 
 use Illuminate\Http\Request;
 
 class LibroController extends Controller
 {
-    
+
     public function index()
     {
-        $libros=Libro::orderBy('id','DESC')->paginate(3);
-        return view('Libro.index',compact('libros')); 
+        $libros = Libro::orderBy('id', 'DESC')->paginate(3);
+        return view('Libro.index', compact('libros'));
     }
 
     public function create()
@@ -19,41 +20,36 @@ class LibroController extends Controller
         return view('Libro.create');
     }
 
-
     public function store(Request $request)
     {
-        $this->validate($request,[ 'nombre'=>'required',  'autor'=>'required']);
+        $this->validate($request, ['nombre' => 'required', 'autor' => 'required']);
         Libro::create($request->all());
-        return redirect()->route('libro.index')->with('success','Registro creado satisfactoriamente'); 
+        return redirect()->route('Libro.index')->with('success', 'Registro creado satisfactoriamente');
     }
-
 
     public function show($id)
     {
-        $libros=Libro::find($id);
-        return  view('libro.show',compact('libros'));
+        $libros = Libro::find($id);
+        return view('Libro.show', compact('libros'));
     }
-
 
     public function edit($id)
     {
-        $libro=libro::find($id);
-        return view('libro.edit',compact('libro'));
+        $libro = libro::find($id);
+        return view('Libro.edit', compact('libro'));
     }
-
 
     public function update(Request $request, $id)
     {
-        $this->validate($request,[ 'nombre'=>'required', 'autor'=>'required']);
- 
+        $this->validate($request, ['nombre' => 'required', 'autor' => 'required']);
+
         libro::find($id)->update($request->all());
-        return redirect()->route('libro.index')->with('success','Registro actualizado satisfactoriamente');
+        return redirect()->route('Libro.index')->with('success', 'Registro actualizado satisfactoriamente');
     }
 
- 
     public function destroy($id)
     {
         Libro::find($id)->delete();
-        return redirect()->route('libro.index')->with('success','Registro eliminado satisfactoriamente');
+        return redirect()->route('Libro.index')->with('success', 'Registro eliminado satisfactoriamente');
     }
 }
--- resources/views/Libro/create.blade.php
+++ resources/views/Libro/create.blade.php
@@ -1,53 +1,123 @@
 @extends('layouts.layout')
 @section('content')
 <div class="row">
-  <section class="content">
-    <div class="col-md-8 col-md-offset-2">
-      <div class="panel panel-default">
-        <div class="panel-body">
-          <div class="pull-left"><h3>Lista Libros</h3></div>
-          <div class="pull-right">
-            <div class="btn-group">
-              <a href="{{ route('libro.create') }}" class="btn btn-info" >Añadir Libro</a>
+	<section class="content">
+		<div class="col-md-8 col-md-offset-2">
+			@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<strong>Error!</strong> Revise los campos obligatorios.<br><br>
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+			@if(Session::has('success'))
+			<div class="alert alert-info">
+				{{Session::get('success')}}
+			</div>
+			@endif
+
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Nuevo Libro</h3>
+				</div>
+				<div class="panel-body">					
+					<div class="table-container">
+						<form method="POST" action="{{ route('Libro.store') }}"  role="form">
+							{{ csrf_field() }}
+							<div class="row">
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="nombre" id="nombre" class="form-control input-sm" placeholder="Nombre del libro">
+									</div>
+								</div>
+								
+							</div>
+                            
+
+							<div class="form-group">
+								<textarea name="autor" class="form-control input-sm" placeholder="Autor"></textarea>
+                            </div>
+                            
+							<div class="row">
+
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<input type="submit"  value="Guardar" class="btn btn-success btn-block">
+									<a href="{{ route('Libro.index') }}" class="btn btn-info btn-block" >Atrás</a>
+								</div>	
+
+							</div>
+						</form>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	@endsection
+
+
+
+
+{{-- @extends('layouts.layout')
+@section('content')
+    <div class="row">
+        <section class="content">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="pull-left">
+                            <h3>Lista Libros</h3>
+                        </div>
+                        <div class="pull-right">
+                            <div class="btn-group">
+                                <a href="{{ route('Libro.create') }}" class="btn btn-info">Añadir Libro</a>
+                            </div>
+                        </div>
+                        <div class="table-container">
+                            <table id="mytable" class="table table-bordred table-striped">
+                                <thead>
+                                    <th>Nombre</th>
+                                    <th>Autor</th>
+                                    <th>Editar</th>
+                                    <th>Eliminar</th>
+                                </thead>
+                                
+                                 <tbody>
+                                    @if ($libros->count())
+                                        @foreach ($libros as $libro)
+                                            <tr>
+                                                <td>{{ $libro->nombre }}</td>
+                                                <td>{{ $libro->autor }}</td>
+                                                <td><a class="btn btn-primary btn-xs"
+                                                        href="{{ action('LibroController@edit', $libros->id) }}"><span
+                                                            class="glyphicon glyphicon-pencil"></span></a></td>
+                                                <td>
+                                                    <form action="{{ action('LibroController@destroy', $libros->id) }}"
+                                                        method="post">
+                                                        {{ csrf_field() }}
+                                                        <input name="_method" type="hidden" value="DELETE">
+
+                                                        <button class="btn btn-danger btn-xs" type="submit"><span
+                                                                class="glyphicon glyphicon-trash"></span></button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <tr>
+                                            <td colspan="8">No hay registro !!</td>
+                                        </tr>
+                                    @endif
+                                </tbody> 
+
+                            </table>
+                        </div>
+                    </div>
+                    {{ $libros->links() }}
+                </div>
             </div>
-          </div>
-          <div class="table-container">
-            <table id="mytable" class="table table-bordred table-striped">
-             <thead>
-               <th>Nombre</th>
-               <th>Autor</th>
-               <th>Editar</th>
-               <th>Eliminar</th>
-             </thead>
-             <tbody>
-              @if($libros->count())  
-              @foreach($libros as $libro)  
-              <tr>
-                <td>{{$libro->nombre}}</td>
-                <td>{{$libro->autor}}</td>
-                <td><a class="btn btn-primary btn-xs" href="{{action('LibroController@edit', $libro->id)}}" ><span class="glyphicon glyphicon-pencil"></span></a></td>
-                <td>
-                  <form action="{{action('LibroController@destroy', $libro->id)}}" method="post">
-                   {{csrf_field()}}
-                   <input name="_method" type="hidden" value="DELETE">
- 
-                   <button class="btn btn-danger btn-xs" type="submit"><span class="glyphicon glyphicon-trash"></span></button>
-                 </td>
-               </tr>
-               @endforeach
-               @else
-               <tr>
-                <td colspan="8">No hay registro !!</td>
-              </tr>
-              @endif
-            </tbody>
- 
-          </table>
-        </div>
-      </div>
-      {{ $libros->links() }}
-    </div>
-  </div>
-</section>
- 
-@endsection
+        </section>
+
+    @endsection --}}
